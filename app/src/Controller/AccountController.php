b1ef137afb162ee8c8f100983da25feb317a8441
Ftug <dlavenu@gmail.com>
2025-02-27 16:00:22 +0000

    Ajout de la gestion des allergies et des notes dans le profil utilisateur

--- app/src/Controller/AccountController.php
+++ app/src/Controller/AccountController.php
@@ -10,6 +10,8 @@ use App\Entity\EmergencyContact;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Allergy;
+use App\Entity\Note;
 
 #[IsGranted('ROLE_USER')]
 class AccountController extends AbstractController
@@ -86,5 +88,93 @@ class AccountController extends AbstractController
         $entityManager->flush();
 
         return new JsonResponse(null, 204);
+    }
+
+    #[Route('/allergy/add', name: 'app_account_allergy_add', methods: ['POST'])]
+    public function addAllergy(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = $this->getUser();
+
+        $allergy = new Allergy();
+        $allergy->setName($data['name']);
+        $allergy->setUser($user);
+
+        $entityManager->persist($allergy);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Allergie ajoutée avec succès']);
+    }
+
+    #[Route('/allergy/{id}/edit', name: 'app_account_allergy_edit', methods: ['POST'])]
+    public function editAllergy(Request $request, Allergy $allergy, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if ($allergy->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $allergy->setName($data['name']);
+
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Allergie modifiée avec succès']);
+    }
+
+    #[Route('/allergy/{id}/delete', name: 'app_account_allergy_delete', methods: ['DELETE'])]
+    public function deleteAllergy(Allergy $allergy, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if ($allergy->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $entityManager->remove($allergy);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Allergie supprimée avec succès']);
+    }
+
+    #[Route('/note/add', name: 'app_account_note_add', methods: ['POST'])]
+    public function addNote(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = $this->getUser();
+
+        $note = new Note();
+        $note->setContent($data['content']);
+        $note->setUser($user);
+
+        $entityManager->persist($note);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Note ajoutée avec succès']);
+    }
+
+    #[Route('/note/{id}/edit', name: 'app_account_note_edit', methods: ['POST'])]
+    public function editNote(Request $request, Note $note, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if ($note->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $note->setContent($data['content']);
+
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Note modifiée avec succès']);
+    }
+
+    #[Route('/note/{id}/delete', name: 'app_account_note_delete', methods: ['DELETE'])]
+    public function deleteNote(Note $note, EntityManagerInterface $entityManager): JsonResponse
+    {
+        if ($note->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $entityManager->remove($note);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Note supprimée avec succès']);
     }
 }
